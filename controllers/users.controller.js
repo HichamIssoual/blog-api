c7a7a4c5c596e2b364a8973be67ed2f78accc736
hichamissoual <hichamissoual.one@gmail.com>
2025-03-25 21:35:17 +0000

    add the basic of update the user controller

--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -6,7 +6,7 @@ const { ERROR, FAIL, SUCCESS } = require("../utils/status.code.text");
  * @access private (only for admin)
  * @description get all users
  * @method GET
- * @end_point "api/users/profile"
+ * @end_point api/users/profile
  ------------------------------------------*/
 const getAllUsers = asynHandler(async (req, res, next) => {
     const users = await Users.find().select("-password").select("-__v");
@@ -19,7 +19,7 @@ const getAllUsers = asynHandler(async (req, res, next) => {
  * @access public
  * @description get user profile
  * @method GET
- * @end_point "api/users/profile/:id"
+ * @end_point api/users/profile/:id
  ------------------------------------------*/
 const getUserProfile = asynHandler(async (req, res, next) => {
     // 1. get id of user
@@ -36,8 +36,34 @@ const getUserProfile = asynHandler(async (req, res, next) => {
         status: SUCCESS,
         user: user
     })
+});
+/**----------------------------------------
+ * @access private
+ * @description update user profile
+ * @method PUT
+ * @end_point api/users/profile/:id
+ ------------------------------------------*/
+const updateUserProfile = asynHandler(async (req, res, next) => {
+    // 1. get id of user
+    const userId = req.params.id;
+    // 2. get the user
+    const oldUser = await Users.findById(userId);
+    // 3. check if user not found
+    if (!oldUser) {
+        // 4. return the error as response
+        const err = errorGenerator.generate(404, FAIL, "user not found");
+        return next(err);
+    }
+    // 5. update the user
+    await Users.updateOne({ _id: userId }, { $set: req.body });
+    // 6. return the response for the client
+    res.status(200).json({
+        status: SUCCESS,
+        message: "updated user successfully",
+    })
 })
 module.exports = {
     getAllUsers,
-    getUserProfile
+    getUserProfile,
+    updateUserProfile
 };
--- routes/users.routes.js
+++ routes/users.routes.js
@@ -1,10 +1,11 @@
 const Router = require("express").Router();
-const { getAllUsers, getUserProfile } = require("../controllers/users.controller");
+const { getAllUsers, getUserProfile, updateUserProfile } = require("../controllers/users.controller");
 const { checkIfIsAdmin } = require("../middlewares/check.is.admin");
 const verifyId = require("../middlewares/verify.id");
 const { verifyToken } = require("../middlewares/verifyToken");
 // /api/users/profile
 Router.route("/profile").get(verifyToken, checkIfIsAdmin, getAllUsers)
 // /api/users/profile/:id
-Router.route("/profile/:id").get(verifyToken, verifyId, getUserProfile);
+Router.route("/profile/:id").get(verifyId, getUserProfile);
+Router.route("/profile/:id").put(verifyToken, verifyId, updateUserProfile);
 module.exports = Router;
